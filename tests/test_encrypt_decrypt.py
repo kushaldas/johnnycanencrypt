d1586fd35c7a5c23cd753b54aa5ba3e0ec7361e6
Kushal Das <mail@kushaldas.in>
2020-10-03 01:12:23 +0530

    Reformats test code via black

--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -1,7 +1,8 @@
 import os
 import johnnycanencrypt as jce
 
-DATA= "Kushal loves 🦀"
+DATA = "Kushal loves 🦀"
+
 
 def clean_outputfiles(output, decrypted_output):
     # Remove any existing test files
@@ -10,6 +11,7 @@ def clean_outputfiles(output, decrypted_output):
     if os.path.exists(decrypted_output):
         os.remove(decrypted_output)
 
+
 def verify_files(inputfile, decrypted_output):
     # read both the files
     with open(inputfile) as f:
@@ -17,14 +19,20 @@ def verify_files(inputfile, decrypted_output):
 
     with open(decrypted_output) as f:
         decrypted_text = f.read()
-    assert original_text ==  decrypted_text
+    assert original_text == decrypted_text
+
 
 def test_encryption_of_multiple_keys():
     "Encrypt bytes to a file using multiple keys"
     output = "/tmp/multiple-enc.asc"
     if os.path.exists(output):
         os.remove(output)
-    jce.encrypt_bytes_to_file(["tests/files/public.asc", "tests/files/hellopublic.asc"], DATA.encode("utf-8"), output.encode("utf-8"), armor=True)
+    jce.encrypt_bytes_to_file(
+        ["tests/files/public.asc", "tests/files/hellopublic.asc"],
+        DATA.encode("utf-8"),
+        output.encode("utf-8"),
+        armor=True,
+    )
     assert os.path.exists(output)
     # Now let us decrypt it via first secret key
     with open(output, "rb") as f:
@@ -46,6 +54,7 @@ def test_encrypt_decrypt_bytes():
     result = jp.decrypt_bytes(enc, "redhat")
     assert DATA == result.decode("utf-8")
 
+
 def test_encrypt_decrypt_bytes_armored():
     "Tests ascii-armored output"
     j = jce.Johnny("tests/files/public.asc")
@@ -54,7 +63,6 @@ def test_encrypt_decrypt_bytes_armored():
     jp = jce.Johnny("tests/files/secret.asc")
     result = jp.decrypt_bytes(enc, "redhat")
     assert DATA == result.decode("utf-8")
-
 
 
 def test_encrypt_decrypt_files():
@@ -68,9 +76,12 @@ def test_encrypt_decrypt_files():
     j = jce.Johnny("tests/files/public.asc")
     assert j.encrypt_file(inputfile.encode("utf-8"), output.encode("utf-8"))
     jp = jce.Johnny("tests/files/secret.asc")
-    assert jp.decrypt_file(output.encode("utf-8"), decrypted_output.encode("utf-8"), "redhat")
+    assert jp.decrypt_file(
+        output.encode("utf-8"), decrypted_output.encode("utf-8"), "redhat"
+    )
 
     verify_files(inputfile, decrypted_output)
+
 
 def test_encrypt_decrypt_files_armored():
     inputfile = "tests/files/text.txt"
@@ -82,7 +93,9 @@ def test_encrypt_decrypt_files_armored():
     j = jce.Johnny("tests/files/public.asc")
     assert j.encrypt_file(inputfile.encode("utf-8"), output.encode("utf-8"), armor=True)
     jp = jce.Johnny("tests/files/secret.asc")
-    assert jp.decrypt_file(output.encode("utf-8"), decrypted_output.encode("utf-8"), "redhat")
+    assert jp.decrypt_file(
+        output.encode("utf-8"), decrypted_output.encode("utf-8"), "redhat"
+    )
 
     with open(output) as f:
         line = f.readline().strip("\n")
--- tests/test_sign_verify_bytes.py
+++ tests/test_sign_verify_bytes.py
@@ -1,12 +1,14 @@
 import pytest
 import johnnycanencrypt as jce
 
-DATA= "Kushal loves 🦀"
+DATA = "Kushal loves 🦀"
+
 
 def test_sign():
     j = jce.Johnny("tests/files/secret.asc")
     signature = j.sign_bytes_detached(DATA.encode("utf-8"), "redhat")
     assert signature
+
 
 def test_verify_bytes():
     j = jce.Johnny("tests/files/secret.asc")
@@ -22,8 +24,8 @@ def test_verify_bytes_must_fail():
     data2 = DATA + " "
     assert not jp.verify_bytes(data2.encode("utf-8"), signature.encode("utf-8"))
 
+
 def test_sign_fail():
     j = jce.Johnny("tests/files/public.asc")
     with pytest.raises(AttributeError):
         signature = j.sign_bytes_detached(DATA.encode("utf-8"), "redhat")
-
