9d17cb203bb6fb7bd71b6276118041f249c4a27e
Kushal Das <mail@kushaldas.in>
2020-10-04 10:31:41 +0530

    Adds a test case and data for #14
    When we encrypt for multiple recipients, we should be able to decrypt
    by any of the recipient's secret key.

--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -102,3 +102,43 @@ def test_encrypt_decrypt_files_armored():
         assert line == "-----BEGIN PGP MESSAGE-----"
 
     verify_files(inputfile, decrypted_output)
+
+
+# The following data was generated while encrypting for 2 UID. Then we will try to decrypt
+# using the second secret key.
+# ❯ gpg2 --homedir=/tmp/a --import tests/files/hellopublic.asc
+# ❯ gpg2 --homedir=/tmp/a --import tests/files/public.asc
+# ❯ gpg2 --homedir=/tmp/a  -r 8ADA07F0A0F7BA99 -r 209940B9669ED621 -e -a > tests/files/double_reipient.asc
+# gpg: WARNING: unsafe permissions on homedir '/tmp/a'
+# gpg: 3CE170115CF4322E: There is no assurance this key belongs to the named user
+
+# sub  rsa4096/3CE170115CF4322E 2020-07-09 Test user <test@gmail.com>
+# Primary key fingerprint: BB2D 3F20 2332 8637 1C31  23D5 2099 40B9 669E D621
+# Subkey fingerprint: 9EBF CA46 5490 663C 22AE  F144 3CE1 7011 5CF4 322E
+
+# It is NOT certain that the key belongs to the person named
+# in the user ID.  If you *really* know what you are doing,
+# you may answer the next question with yes.
+
+# Use this key anyway? (y/N) y
+# gpg: 76E7E83323D9A3AF: There is no assurance this key belongs to the named user
+
+# sub  rsa4096/76E7E83323D9A3AF 2020-10-02 test key
+# Primary key fingerprint: 6AC6 957E 2589 CB8B 5221  F650 8ADA 07F0 A0F7 BA99
+# Subkey fingerprint: 3080 B349 B5C2 CF8E 1869  B2F5 76E7 E833 23D9 A3AF
+
+# It is NOT certain that the key belongs to the person named
+# in the user ID.  If you *really* know what you are doing,
+# you may answer the next question with yes.
+
+# Use this key anyway? (y/N) y
+# Hello World! for 2.
+
+# Test case for issue number #14
+def test_decrypt_multiple_recipient_data():
+    with open("tests/files/double_recipient.asc", "rb") as f:
+        data = f.read()
+
+    jp = jce.Johnny("tests/files/secret.asc")
+    cleartext = jp.decrypt_bytes(data, "redhat")
+    assert cleartext == b"Hello World! for 2.\n"
