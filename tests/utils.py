386e9ad55e72fcffbee71303518fb797c3c64b1f
Kushal Das <mail@kushaldas.in>
2020-10-06 00:26:27 +0530

    Cleans up the tests a bit

---
+++ tests/utils.py
@@ -0,0 +1,19 @@
+import os
+
+
+def clean_outputfiles(output, decrypted_output):
+    # Remove any existing test files
+    if os.path.exists(output):
+        os.remove(output)
+    if os.path.exists(decrypted_output):
+        os.remove(decrypted_output)
+
+
+def verify_files(inputfile, decrypted_output):
+    # read both the files
+    with open(inputfile) as f:
+        original_text = f.read()
+
+    with open(decrypted_output) as f:
+        decrypted_text = f.read()
+    assert original_text == decrypted_text
