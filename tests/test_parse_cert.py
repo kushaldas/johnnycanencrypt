5678af11e5286505e84ef3ccc63d371f79840b00
Kushal Das <mail@kushaldas.in>
2020-10-18 19:18:16 +0530

    Adds function to test merge_keys rust implementation
    Uses kushal's old key and the updated key

--- tests/test_parse_cert.py
+++ tests/test_parse_cert.py
@@ -38,3 +38,29 @@ def test_parse_cert_bytes():
     )
     assert etime.date() == expirationtime.date()
     assert ctime.date() == creationtime.date()
+
+def test_merge_certs():
+    """Tests the rust implementation of merging two OpenPGP keys.
+
+    Tests via Kushal's old key, and the new key
+    """
+    # These two are known values from kushal
+    ctime = datetime.datetime(2017, 10, 17, 20, 53, 47)
+    # First let us read from the file
+    keypath = "tests/files/store/pgp_keys.asc"
+    with open(keypath, "rb") as fobj:
+        data = fobj.read()
+
+    keypath = "tests/files/store/kushal_updated_key.asc"
+    with open(keypath, "rb") as fobj:
+        newdata = fobj.read()
+
+    newcert = rustjce.merge_keys(data, newdata)
+    assert isinstance(newcert, bytes)
+
+    uids, fingerprint, keytype, expirationtime, creationtime = rustjce.parse_cert_bytes(
+        newcert
+    )
+
+    assert ctime.date() == creationtime.date()
+    assert not expirationtime
