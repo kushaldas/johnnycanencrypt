621514b41be9d7a460828522e6f60e23bf8f91cb
Kushal Das <mail@kushaldas.in>
2025-11-26 18:13:06 +0100

    test build for linux manylinux

---
+++ build-wheels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+yum install -y nettle clang clang-devel nettle-devel pcsc-lite-devel
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv --python /opt/python/cp314-cp314/bin/python3.14 /opt/venv
+source /opt/venv/bin/activate
+uv pip install --upgrade "maturin>=1,<2" -r /io/requirements-dev.txt
+cd /io/
+maturin build --release --manylinux --sdist
+mkdir dist/
+echo "Now we need to fix the libpcsc as Debian/Ubuntu/Fedora needs to load system's current library."
+for whl in target/wheels/johnnycanencrypt*cp310*-manylinux*.whl; do
+	mkdir wtest
+	unzip "$whl" -d wtest/
+	patchelf --replace-needed libpcsclite-a573614e.so.1.0.0 libpcsclite.so.1 wtest/johnnycanencrypt/johnnycanencrypt.abi3.so
+	wheel pack --dest-dir ./dist/ wtest
+	rm -rf wtest
+done
+echo "Let us copy the source tarball"
+cp target/wheels/johnnycanencrypt*.tar.gz ./dist/
